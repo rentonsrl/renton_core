251798fda3f2572cb790d00807f763bbf82f37e9
rentonsrl <86151958+rentonsrl@users.noreply.github.com>
2022-09-14 18:46:58 +0530

    Add files via upload

---
+++ Renton/Core/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ *
+ * Glory to Ukraine! Glory to the heroes!
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Renton_Core',
+    __DIR__
+);
